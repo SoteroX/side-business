10350eecdf41837a494b54175f1719762b66d3f2
SoteroX <soterox13@gmail.com>
2018-10-06 18:06:20 -0500

    changed feature design

--- src/components/homepage/feature/Feature.jsx
+++ src/components/homepage/feature/Feature.jsx
@@ -2,55 +2,83 @@ import React from 'react';
 
 export default () => {
   return (
-    <div className="feature-section page-section grey-light-bg clearfix">
-      <div className="fes7-img-cont col-lg-5 col-md-12">
-        <div className="fes7-img"></div>
-      </div>
+    <div className="feature-section page-section fes4-cont">
       <div className="container">
         <div className="row">
-          <div className="col-lg-6 col-lg-offset-6 fes7-text-cont p-80-cont ml-auto col-md-12 col-md-offset-0 ">
-            <h1><span className="font-light">Nulla varius faucibus vestibulum. Sed imperdiet, tellus at iaculis</span></h1>
-            <p className="mb-60">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
-            </p>
-            <div className="row">
-              <div className="col-md-6 col-sm-6">
-                <div className="fes7-box wow fadeIn" >
-                  <div className="fes7-box-icon">
-                    <div><i className="fas fa-chart-line"></i></div>
-                  </div>
-                  <h3>SEO Friendly</h3>
-                  <p>Sed ut perspiciatis unde omnis iste nat eror acus  antium que</p>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <div className="fes4-box wow fadeIn">
+              <h2 className="section-title">OUR <span className="bold">SERVICES</span></h2>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <div className="fes4-box wow fadeIn" data-wow-delay="200ms">
+              <div className="fes4-title-cont" >
+                <div className="fes4-box-icon">
+                  <i className="fas fa-wrench"></i>
                 </div>
+                <h3><span className="bold">DEVELOPMENT</span></h3>
+                <p>LOREM IPSUM DOLOR</p>
               </div>
-              <div className="col-md-6 col-sm-6">
-                <div className="fes7-box wow fadeIn" data-wow-delay="200ms">
-                  <div className="fes7-box-icon">
-                    <div><i className="fas fa-font"></i></div>
-                  </div>
-                  <h3>Google Fonts</h3>
-                  <p>Sed ut perspiciatis unde omnis iste nat eror acus  antium que</p>
-                </div>
+              <div>
+                  Maecenas luctus nisi in sem fermentum blat. In nec  elit solliudin, elementum, dictum pur quam volutpat suscipit antena.
               </div>
             </div>
-            <div className="row">
-              <div className="col-md-6 col-sm-6">
-                <div className="fes7-box wow fadeIn" data-wow-delay="400ms">
-                  <div className="fes7-box-icon">
-                    <div><i className="fas fa-mobile-alt"></i></div>
-                  </div>
-                  <h3>Mobile Development</h3>
-                  <p>Sed ut perspiciatis unde omnis iste nat eror acus  antium que</p>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <div className="fes4-box wow fadeIn" data-wow-delay="400ms">
+              <div className="fes4-title-cont" >
+                <div className="fes4-box-icon">
+                  <i className="fas fa-share-alt"></i>
                 </div>
+                <h3><span className="bold">PRODUCTION</span></h3>
+                <p>LOREM IPSUM DOLOR</p>
               </div>
-              <div className="col-md-6 col-sm-6">
-                <div className="fes7-box wow fadeIn" data-wow-delay="600ms">
-                  <div className="fes7-box-icon">
-                    <div><i className="fab fa-fort-awesome"></i></div>
-                  </div>
-                  <h3>1500+ Icons</h3>
-                  <p>Sed ut perspiciatis unde omnis iste nat eror acus  antium que</p>
+              <div>
+                  Seductio maesto nisi in sem fermentum blat. In nec elit solliudin, elementum, dictum pur quam volutpat suscipit antena.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <div className="fes4-box wow fadeIn" data-wow-delay="600ms">
+              <div className="fes4-title-cont" >
+                <div className="fes4-box-icon">
+                  <i className="fas fa-bullseye"></i>
                 </div>
+                <h3><span className="bold">BRANDING</span></h3>
+                <p>LOREM IPSUM DOLOR</p>
+              </div>
+              <div>
+                Donec vel luctus nisi in sem fermentum blat. In nec elit solliudin, elementum, dictum pur quam volutpat suscipit antena.
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <div className="fes4-box wow fadeIn" data-wow-delay="800ms" >
+              <div className="fes4-title-cont" >
+                <div className="fes4-box-icon">
+                  <i className="fas fa-globe-americas"></i>
+                </div>
+                <h3><span className="bold">WEB DESIGN</span></h3>
+                <p>LOREM IPSUM DOLOR</p>
+              </div>
+              <div>
+                Lorem luctus antena at nisi in sem blandit. In nec elit soltudin, elementum odio et, dictum quam a volutpat elementum.
+              </div>
+            </div>
+          </div>
+          <div className="col-xs-12 col-sm-4 col-md-4">
+            <div className="fes4-box wow fadeIn" data-wow-delay="1000ms">
+              <div className="fes4-title-cont" >
+                <div className="fes4-box-icon">
+                  <i className="fas fa-mobile-alt"></i>
+                </div>
+                <h3><span className="bold">Mobile Development</span></h3>
+                <p>LOREM IPSUM DOLOR</p>
+              </div>
+              <div>
+                Fermentum nisi in sem fertum blat. In elit soldin, elemtum, arenam pur quam volutpat suscipit dictum pur quam.
               </div>
             </div>
           </div>
